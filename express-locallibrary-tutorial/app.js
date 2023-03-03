aaa69e80b23c846167cb7c9376ffe6096b6ed234
James Graca-Jones <jamesgracajones@gmail.com>
2023-03-03 17:07:14 +0000

    Write connection to MongoDB database

--- express-locallibrary-tutorial/app.js
+++ express-locallibrary-tutorial/app.js
@@ -9,6 +9,16 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+// Set up mongoose connection
+const mongoose = require("mongoose");
+mongoose.set('strictQuery', false);
+const mongoDB = "mongodb+srv://jamesgracajones:9b3h9YAwSf3X44OO@cluster0.nepyass.mongodb.net/?retryWrites=true&w=majority";
+
+main().catch(err => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
