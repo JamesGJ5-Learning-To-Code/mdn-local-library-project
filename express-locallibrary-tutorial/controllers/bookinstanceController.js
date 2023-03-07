74274fa19bb771c4894900f4bc576f5075558041
James Graca-Jones <jamesgracajones@gmail.com>
2023-03-07 19:35:46 +0000

    Write controller to fill bookinstance_detail view

--- express-locallibrary-tutorial/controllers/bookinstanceController.js
+++ express-locallibrary-tutorial/controllers/bookinstanceController.js
@@ -18,8 +18,23 @@ exports.bookinstance_list = function (req, res, next) {
 };
 
 // Display detail page for a specific BookInstance.
-exports.bookinstance_detail = (req, res) => {
-  res.send(`NOT IMPLEMENTED: BookInstance detail: ${req.params.id}`);
+exports.bookinstance_detail = (req, res, next) => {
+  BookInstance.findById(req.params.id)
+    .populate("book")
+    .then((bookinstance) => {
+      if (bookinstance == null) {
+        const err = new Error("Book copy not found");
+        err.status = 404;
+        return next(err);
+      }
+      res.render("bookinstance_detail", {
+        title: `Copy: ${bookinstance.book.title}`,
+        bookinstance,
+      });
+    })
+    .catch((err) => {
+      return next(err);
+    });
 };
 
 // Display BookInstance create form on GET.
