68e8dd0a33c1c81daa77671d8cd0a3555b5e53f3
James Graca-Jones <jamesgracajones@gmail.com>
2023-03-07 16:01:37 +0000

    Write controller for displaying book instances in bookinstance_list

--- express-locallibrary-tutorial/controllers/bookinstanceController.js
+++ express-locallibrary-tutorial/controllers/bookinstanceController.js
@@ -1,8 +1,20 @@
 const BookInstance = require("../models/bookinstance");
 
 // Display list of all BookInstances.
-exports.bookinstance_list = (req, res) => {
-  res.send("NOT IMPLEMENTED: BookInstance list");
+exports.bookinstance_list = function (req, res, next) {
+  BookInstance.find()
+    .populate("book")
+    .then(function (list_bookinstances) {
+      res.render("bookinstance_list", {
+        title: "Book Instance List",
+        bookinstance_list: list_bookinstances,
+      });
+    })
+    .catch(function (err) {
+      if (err) {
+        return next(err);
+      }
+    });
 };
 
 // Display detail page for a specific BookInstance.
