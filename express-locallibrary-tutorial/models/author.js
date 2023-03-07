d5965d4e5e90fd1d59ffec24d4ec0c5ca8d6806b
James Graca-Jones <jamesgracajones@gmail.com>
2023-03-07 18:02:24 +0000

    Write virtual property to store formatted DOB if there is a DOB

--- express-locallibrary-tutorial/models/author.js
+++ express-locallibrary-tutorial/models/author.js
@@ -1,3 +1,5 @@
+const { DateTime } = require("luxon");
+
 const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
@@ -27,6 +29,13 @@ AuthorSchema.virtual("name").get(function () {
 AuthorSchema.virtual("url").get(function () {
   // We don't use an arrow function as we'll need the this object
   return `/catalog/author/${this._id}`;
+});
+
+AuthorSchema.virtual("date_of_birth_formatted").get(function () {
+  return this.date_of_birth ? 
+    DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED)
+    :
+    '';
 });
 
 // Export model
