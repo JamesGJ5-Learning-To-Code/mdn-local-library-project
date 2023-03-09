85a59c342b1a9c17d9c2bc8f402f397430d932f6
James Graca-Jones <jamesgracajones@gmail.com>
2023-03-09 13:16:24 +0000

    Write controller for handling POST from bookinstance form view

--- express-locallibrary-tutorial/controllers/bookinstanceController.js
+++ express-locallibrary-tutorial/controllers/bookinstanceController.js
@@ -55,9 +55,60 @@ exports.bookinstance_create_get = (req, res, next) => {
 };
 
 // Handle BookInstance create on POST.
-exports.bookinstance_create_post = (req, res) => {
-  res.send("NOT IMPLEMENTED: BookInstance create POST");
-};
+exports.bookinstance_create_post = [
+  // Validate and sanitize fields.
+  body("book", "Book must be specified").trim().isLength({ min: 1 }).escape(),
+  body("imprint", "Imprint must be specified")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("status").escape(),
+  body("due_back", "Invalid date")
+    .optional({ checkFalsy: true })
+    .isISO8601()
+    .toDate(),
+
+  // Process request after validation and sanitization.
+  (req, res, next) => {
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+
+    // Create a BookInstance object with escaped and trimmed data.
+    const bookinstance = new BookInstance({
+      book: req.body.book,
+      imprint: req.body.imprint,
+      status: req.body.status,
+      due_back: req.body.due_back,
+    });
+
+    if (!errors.isEmpty()) {
+      // There are errors. Render form again with sanitized values and error messages.
+      Book.find({}, "title").then(function (books) {
+        // Successful, so render.
+        res.render("bookinstance_form", {
+          title: "Create BookInstance",
+          book_list: books,
+          selected_book: bookinstance.book._id,
+          errors: errors.array(),
+          bookinstance,
+        });
+      })
+      .catch(function (err) {
+        return next(err);
+      });
+      return;
+    }
+
+    // Data from form is valid
+    bookinstance.save().then(() => {
+      // Successful: redirect to new record.
+      res.redirect(bookinstance.url);
+    })
+    .catch((err) => {
+      return next(err);
+    });
+  },
+];
 
 // Display BookInstance delete form on GET.
 exports.bookinstance_delete_get = (req, res) => {
